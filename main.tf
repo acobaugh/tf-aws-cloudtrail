c9edb8c7bf4bf855ac1eeeef9e100b67930986da
Andy Cobaugh <andrew.cobaugh@gmail.com>
2018-04-05 23:08:30 -0400

    make sure bucket is created before the trail

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 resource "aws_cloudtrail" "cloudtrail" {
   name                          = "cloudtrail"
-  s3_bucket_name                = "${var.s3_bucket_name}"
+  s3_bucket_name                = "${aws_s3_bucket.id}"
   tags                          = "${var.tags}"
   is_multi_region_trail         = "${var.is_multi_region_trail}"
   include_global_service_events = "${var.include_global_service_events}"
